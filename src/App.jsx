c601ccd144e8d35fa8af1cce12886b64ae3211ea
bokkle <mitch.r_@hotmail.com>
2024-05-30 21:48:37 -0400

    .

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./navigation/Navigation";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+    return (
+        <header className="border h-[100px]">
+            <div className="bg-blue-400 w-full h-full">
+            </div>            
+        </header>
+    )
+}
+
+export default Navigation
